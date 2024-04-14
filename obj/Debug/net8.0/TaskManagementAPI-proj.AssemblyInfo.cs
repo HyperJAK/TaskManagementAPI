4725af6d5d4b7128c49ee1fdf3e1782e00d60efb
HyperJAK <james.jakthesamething@gmail.com>
2024-04-15 02:05:04 +0300

    Added logic to add a user to a project and authorisation to restrict him from certain things

--- obj/Debug/net8.0/TaskManagementAPI-proj.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagementAPI-proj.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementAPI-proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc85620bd4224114033a71aa33e53825661f5c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e63bf60aae59f7211ad2ca0350c81ec3714a5eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementAPI-proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementAPI-proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
